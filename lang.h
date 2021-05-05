043d0286fa35578e86b24b087befa75630b296cf
Matías Israelson <57065102+israpps@users.noreply.github.com>
2021-05-05 15:08:48 -0300

    Update lang.h

--- lang.h
+++ lang.h
@@ -1,12 +1,11 @@
 #ifdef WLE_LANG_SPA
-#define CUSTOM_LNG
 #include "Lang/SPA.LNG"
 #endif
 
-#ifdef WLE_LANG_ENG
-#define CUSTOM_LNG
-#include "Lang/ENG.LNG"
-#endif
+//#ifdef WLE_LANG_ENG
+//#define CUSTOM_LNG
+//#include "Lang/ENG.LNG"
+//#endif
 
 #ifndef CUSTOM_LNG
 #include "Lang/ENG.LNG"
