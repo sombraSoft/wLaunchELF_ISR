eea7a62c3643ef6f16ef09296ade11d570418f5d
Matías Israelson <57065102+israpps@users.noreply.github.com>
2021-12-27 20:14:17 -0300

    Update main.c

--- main.c
+++ main.c
@@ -839,9 +839,9 @@ static void ShowDebugInfo(void)
 				sprintf(TextRow, "argv[%d] == \"%s\"", i, boot_argv[i]);
 				PrintRow(-1, TextRow);
 			}
-			sprintf(TextRow, "System Update Folder == ",GetMGFolderLetter(ROMVER_data[4]));
+			sprintf(TextRow, "System Update Folder == %s",GetMGFolderLetter(ROMVER_data[4]));
 			PrintRow(-1, TextRow);
-			sprintf(TextRow, "System Update KELF == ",strrchr(default_OSDSYS_path,'/') + 1);
+			sprintf(TextRow, "System Update KELF == %s",strrchr(default_OSDSYS_path,'/') + 1);
 			PrintRow(-1, TextRow);
 			sprintf(TextRow, "boot_path == \"%s\"", boot_path);
 			PrintRow(-1, TextRow);
